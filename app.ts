34439e67329df91174153948f0c9345eb22cd90d
Sobirjon <ahmadjonovsobirjon24@gmail.com>
2025-04-29 18:28:29 +0500

    updated

--- app.ts
+++ app.ts
@@ -2,18 +2,18 @@ import "reflect-metadata";
 
 // 1-mashq
 
-function LogClass(target: any) {
-  const className = target.name;
-  const currentTime = new Date().toISOString();
-  console.log(`${className} class yaratildi vahti ${currentTime}`);
-}
+// function LogClass(target: any) {
+//   const className = target.name;
+//   const currentTime = new Date().toISOString();
+//   console.log(`${className} class yaratildi vahti ${currentTime}`);
+// }
 
-@LogClass
-class User {
-  constructor(public name: string, public age: number) {}
-}
+// @LogClass
+// class User {
+//   constructor(public name: string, public age: number) {}
+// }
 
-// 2-mashq
+// // 2-mashq
 
 function ReadOnly(target: any, propertyKey: string) {
   const descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
@@ -24,18 +24,19 @@ function ReadOnly(target: any, propertyKey: string) {
 }
 
 class Product {
-  //@ts-ignore
-  @ReadOnly
   public name: string;
   constructor(name: string, public price: number) {
     this.name = name;
   }
+  //@ts-ignore
+  @ReadOnly
   changeName(newName: string) {
     this.name = newName;
   }
 }
 const newProduct = new Product("Samsung A22", 1200);
 newProduct.changeName("Samsung A21");
+console.log(newProduct);
 
 // 3-mashq
 
@@ -59,51 +60,49 @@ function TimeMeasure(
 }
 
 class DataProcessor {
-  //@ts-ignore
   @TimeMeasure
   processData(data: number[]) {
     for (let i = 0; i < 1000000; i++) {
       data = data.map((x) => x * 2);
-      if (i % 100000 === 0) break;
     }
     return data;
   }
 }
 
 const newProcess = new DataProcessor();
-newProcess.processData([10]);
+newProcess.processData([1, 2, 3, 4, 5]);
 
 // 4-mashq
 
-function ValidatePositive(
-  target: any,
-  methodName: string,
-  parameterIndex: number
-) {
-  const existingValidation: any[] =
-    Reflect.getOwnMetadata("design:paramtypes", target, methodName) || [];
-  existingValidation[parameterIndex] = (value: any) => {
-    if (value <= 0) {
-      throw new Error(`${methodName} parametr musbat bo'lishi kerak`);
-    }
-  };
-  Reflect.defineMetadata(
-    "design:paramtypes",
-    existingValidation,
-    target,
-    methodName
-  );
-}
+// function ValidatePositive(
+//   target: any,
+//   methodName: string,
+//   parameterIndex: number
+// ) {
+//   const existingValidation: any[] =
+//     Reflect.getOwnMetadata("design:paramtypes", target, methodName) || [];
+//   existingValidation[parameterIndex] = (value: any) => {
+//     if (value <= 0) {
+//       throw new Error(`${methodName} parametr musbat bo'lishi kerak`);
+//     }
+//   };
+//   Reflect.defineMetadata(
+//     "design:paramtypes",
+//     existingValidation,
+//     target,
+//     methodName
+//   );
+// }
 
-class BankAccount {
-  //@ts-ignore
-  @ValidatePositive
-  private balance: number = 0;
-  deposit(amount: number) {
-    this.balance += amount;
-    return this.balance;
-  }
-}
+// class BankAccount {
+//   //@ts-ignore
+//   @ValidatePositive
+//   private balance: number = 0;
+//   deposit(amount: number) {
+//     this.balance += amount;
+//     return this.balance;
+//   }
+// }
 
-const account = new BankAccount();
-console.log(account.deposit(100));
+// const account = new BankAccount();
+// console.log(account.deposit(100));
